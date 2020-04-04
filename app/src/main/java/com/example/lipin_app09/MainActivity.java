1ba62d4c7523d694a6fded922dcade69986e077d
蕭立品 <a0979225002@gmail.com>
2020-04-04 19:39:03 +0800

    存取檔案功能lipin_app

--- app/src/main/java/com/example/lipin_app09/MainActivity.java
+++ app/src/main/java/com/example/lipin_app09/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
 
 import ir.sohreco.androidfilechooser.ExternalStorageNotAvailableException;
 import ir.sohreco.androidfilechooser.FileChooser;
@@ -43,12 +46,13 @@ public class MainActivity extends AppCompatActivity
     public void test1(View view) {
         FileChooser.Builder builder = new FileChooser.Builder(
                 FileChooser.ChooserType.FILE_CHOOSER, this)
-                .setMultipleFileSelectionEnabled(true)
-                .setFileFormats(new String[] {".jpg", ".png"})
+                .setMultipleFileSelectionEnabled(false)//多重選擇文件按鈕
+                .setFileFormats(new String[] {".jpg"})
                 .setListItemsTextColor(R.color.colorPrimary)
                 .setPreviousDirectoryButtonIcon(R.drawable.ic_prev_dir)
                 .setDirectoryIcon(R.drawable.ic_directory)
                 .setFileIcon(R.drawable.ic_file)
+
                 // And more...
                 ;
 
@@ -56,7 +60,9 @@ public class MainActivity extends AppCompatActivity
         try {
             FileChooser fileChooserFragment = builder.build();
             getSupportFragmentManager().beginTransaction()
+                    //建立一個FrameLayout 將Fragment放入
                     .add(R.id.container, fileChooserFragment)
+                    //commit()開始執行Fragment的切換
                     .commit();
 
         } catch (ExternalStorageNotAvailableException e) {
@@ -67,12 +73,40 @@ public class MainActivity extends AppCompatActivity
 
     //上傳檔案按鈕
     public void upload(View view) {
+        new Thread(){
+            @Override
+            public void run() {
+                doupload();
+            }
+        }.start();
+    }
+    //上傳方法
+    private void doupload(){
+       try {
 
+        MultipartUtility mu = new MultipartUtility(
+                "http://192.168.1.181:8080/lipin01",
+                "",
+                "UTF-8");
+        //使用html中命名的表單名稱欄位
+           //html功能更換檔案的名子
+        mu.addFormField("prefix","android");
+        //使用html中命名的檔案上傳欄位
+        mu.addFilePart("upload", uploadFile);
+
+        mu.finish();
+        Log.v("brad","upload ok");
+
+       }catch (Exception e){
+        Log.v("brad",e.toString());
+       }
     }
 
-
+    //拿取上傳檔案的路徑
+    File uploadFile;
     @Override
     public void onSelect(String path) {
-
+       uploadFile = new File(path);
+        Log.v("brad",path);
     }
 }
